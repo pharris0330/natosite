9cd3b0187b4186d60c95f397849256cc4764975b
Paul Harris <pharris0330@gmail.com>
2025-01-12 12:39:25 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+# Title for the Streamlit app
+st.title("NATO cliffnotes")
+
+# Display a paragraph
+st.write(
+    """
+    This is for all things NATO/Navy. Post will be populated periodically. Goal is for us to be aware of all big ticket items related to NATO.
+    """
+)
+
+st.write(
+    """
+    ### CNO NAVPLAN 2024
+    - **Navigation Plan 2024 outlines the strategic guidance for the Navy, focusing on readiness for potential conflict with the People's Republic of China by 2027 and enhancing the Navy's long-term advantage.
+    - **Project 33 is a key initiative to accelerate readiness efforts, with targets set to eliminate maintenance delays, scale robotic systems, create command centers, recruit and retain personnel, improve service quality, train for combat, and restore critical infrastructure.
+    - **The plan emphasizes the importance of adapting to a changing security environment and technological advancements, with a focus on integrating robotic and autonomous systems, enhancing information warfare capabilities, and preparing for high-end warfare.
+    - **Navy's progress includes addressing force challenges, constraints, and the need for sustained budget growth to modernize and expand the fleet.
+    - **The plan highlights the Navy's commitment to building warfighters, improving training, and ensuring a quality of service for Sailors, as well as restoring critical infrastructure to support operational readiness.
+    - **Expansion of the Navy's contribution to the Joint warfighting ecosystem is a priority, focusing on key capabilities like long-range fires, non-traditional sea denial, counter-C5ISRT, terminal defense, contested logistics, and enablers such as training, operational architecture, artificial intelligence, and robotic autonomous systems.
+    - **The plan sets the course for the future Navy beyond 2027, focusing on raising readiness levels permanently, adapting to evolving threats, and delivering a Navy that is always ready and capable of achieving overwhelming advantage.
+    """
+
+)
+
+# Display a bulleted list with links
+st.markdown("""
+### Key Features of Streamlit
+- **[NATO website1](https://docs.streamlit.io/library/api-reference/widgets):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+- **[NATO website2](https://docs.streamlit.io/library/api-reference/write-magic/st.markdown):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+- **[GO NAVY!!](https://docs.streamlit.io/library/api-reference/charts):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+- **[Beat Army!!](https://streamlit.io/cloud):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+""")
+
+# Another paragraph with a hyperlink
+st.write("Good luck becoming a NATO master!")
