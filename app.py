5f59420c0b4aecb86a2a2b4f12f8f7b628c297a5
Paul Harris <pharris0330@gmail.com>
2025-01-12 13:22:22 -0500

    third commit

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ st.title("NATO cliffnotes")
 # Display a paragraph
 st.write(
     """
-    This is for all things NATO/Navy. Post will be populated periodically. Goal is for us to be aware of all big ticket items related to NATO.
+    This is for all things NATO/Navy. Post will be populated periodically. Goal is for us to be aware of all big ticket items related to NATO. The below are bulleted summaries of major documents related to NATO.
     """
 )
 
 st.write(
     """
-    ### CNO NAVPLAN 2024 (30 pages)
+    ###[CNO NAVY plan 2024](https://www.navy.mil/Leadership/Chief-of-Naval-Operations/CNO-NAVPLAN-2024/):** (30 pages)
     - **Navigation Plan 2024 outlines the strategic guidance for the Navy, focusing on readiness for potential conflict with the People's Republic of China by 2027 and enhancing the Navy's long-term advantage.
     - **Project 33 is a key initiative to accelerate readiness efforts, with targets set to eliminate maintenance delays, scale robotic systems, create command centers, recruit and retain personnel, improve service quality, train for combat, and restore critical infrastructure.
     - **The plan emphasizes the importance of adapting to a changing security environment and technological advancements, with a focus on integrating robotic and autonomous systems, enhancing information warfare capabilities, and preparing for high-end warfare.
@@ -21,13 +21,34 @@ st.write(
     - **Expansion of the Navy's contribution to the Joint warfighting ecosystem is a priority, focusing on key capabilities like long-range fires, non-traditional sea denial, counter-C5ISRT, terminal defense, contested logistics, and enablers such as training, operational architecture, artificial intelligence, and robotic autonomous systems.
     - **The plan sets the course for the future Navy beyond 2027, focusing on raising readiness levels permanently, adapting to evolving threats, and delivering a Navy that is always ready and capable of achieving overwhelming advantage.
     """
+)
+    
+st.write(    
+    """
+    ###[North Atlantic Treaty](https://www.nato.int/nato_static_fl2014/assets/pdf/stock_publications/20120822_nato_treaty_en_light_2009.pdf): ** (18 pages)
 
+    - **The North Atlantic Treaty was signed in Washington, D.C., on April 4, 1949.
+    - **The Treaty emphasizes the principles of democracy, individual liberty, and the rule of law.
+    - **The Parties commit to settling disputes peacefully and refraining from using force inconsistent with the UN Charter.
+    - **They agree to collective defense, consultation in case of threats, and responding to armed attacks.
+    - **An attack on one Party is considered an attack on all Parties, necessitating collective action.
+    - **The Treaty came into force on August 24, 1949, after ratification by key signatory states.
+    - **The Treaty can be reviewed after ten years and a Party can cease to be a member after twenty years.
+    - **Additional Protocols allowed the accession of countries like Greece, Turkey, Germany, Spain, and others.
+    - **Accession Protocols detailed the process for new countries to join the North Atlantic Treaty.
+    - **The Treaty's purpose is to promote stability, well-being, and the security of the North Atlantic area.
+    - **Parties commit to maintaining and developing their capacity to resist armed attacks.
+    - **The Treaty does not affect the rights and obligations of UN member countries or the Security Council's role.
+    - **Countries ratify the Treaty according to their constitutional processes.
+    - **The establishment of a Council ensures continuous consultation and defense measures.
+    - **The Treaty is committed to promoting peaceful international relations and economic collaboration.
+    """
 )
 
 # Display a bulleted list with links
 st.markdown("""
-### Key Features of Streamlit
-- **[NATO website1](https://docs.streamlit.io/library/api-reference/widgets):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+### Key NATO/NAY
+- **[CNO NAVY plan 2024](https://docs.streamlit.io/library/api-reference/widgets):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
 - **[NATO website2](https://docs.streamlit.io/library/api-reference/write-magic/st.markdown):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
 - **[GO NAVY!!](https://docs.streamlit.io/library/api-reference/charts):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
 - **[Beat Army!!](https://streamlit.io/cloud):** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
